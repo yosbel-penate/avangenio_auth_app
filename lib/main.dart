18421fd40b3de9746c8dd360a26b8e4fbedcc0f4
Corso <58090876+yosbel-penate@users.noreply.github.com>
2024-01-14 23:39:13 -0500

    unit tests are runing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// main.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './providers/auth_provider.dart';
--- lib/test/auth_test.dart
+++ lib/test/auth_test.dart
@@ -1,13 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import '../providers/auth_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:mockito/mockito.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   group('Authentication Tests', () {
     test('Login and Logout', () async {
       AuthProvider authProvider = AuthProvider();
-
+      //crear una instance de SharedPreferences para test
+      final mockPrefs = MockSharedPreferences();
+      SharedPreferences.setMockInitialValues({});
       // Login test
-      await authProvider.login('testuser', 'testpassword', 'hellp@um.cp');
+      await authProvider.login('corso', '123', 'hellp@um.cp');
       expect(authProvider.isAuthenticated, true);
 
       // Logout test
@@ -17,7 +26,9 @@ void main() {
 
     test('Save and Load User from SharedPreferences', () async {
       AuthProvider authProvider = AuthProvider();
-
+      //crear una instance de SharedPreferences para test
+      final mockPrefs = MockSharedPreferences();
+      SharedPreferences.setMockInitialValues({});
       // Save user to SharedPreferences
       await authProvider.saveUserToPrefs();
 
@@ -29,7 +40,9 @@ void main() {
 
     test('Remove User from SharedPreferences', () async {
       AuthProvider authProvider = AuthProvider();
-
+      //crear una instance de SharedPreferences para test
+      final mockPrefs = MockSharedPreferences();
+      SharedPreferences.setMockInitialValues({});
       // Remove user from SharedPreferences
       await authProvider.removeUserFromPrefs();
 
